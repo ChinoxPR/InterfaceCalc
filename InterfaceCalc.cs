562495e4a193c98b01a723ca7f021d024511aa74
johnc <johnc@DESKTOP-R443UCS>
2021-08-04 16:10:42 -0700

    Add project files.

---
+++ InterfaceCalc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceCalc
+{
+    interface ICalc
+    {
+        void Add(int i, int j);
+
+        void Subtract()
+
+
+
+    }
+
+
+    class Calculator
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
